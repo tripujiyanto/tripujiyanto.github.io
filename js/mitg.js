abee1789325a3f575c5678409245d432c529af5b
tripujiyanto <pujigaopy@gmail.com>
2021-08-06 11:56:07 +0700

    mega update
    update:
    - menu hover
    - footer
    - bg
    - about
    dll

--- js/mitg.js
+++ js/mitg.js
@@ -31,27 +31,13 @@ $(document).ready(function () {
   }
 });
 
-// parralax--------------------------->start
-
+// menu--------------------------->start
 document.addEventListener("DOMContentLoaded", function () {
-  var elems = document.querySelectorAll(".parallax");
-  var instances = M.Parallax.init(elems, options);
+  /////// Prevent closing from click inside dropdown
+  document.querySelectorAll(".dropdown-menu").forEach(function (element) {
+    element.addEventListener("click", function (e) {
+      e.stopPropagation();
+    });
+  });
 });
-
-// Or with jQuery
-
-$(document).ready(function () {
-  $(".parallax").parallax();
-});
-
-var instance = M.Parallax.getInstance(elem);
-
-/* jQuery Method Calls
-  You can still use the old jQuery plugin method calls.
-  But you won't be able to access instance properties.
-
-  $('.parallax').parallax('methodName');
-  $('.parallax').parallax('methodName', paramName);
-*/
-
-instance.destroy();
+// DOMContentLoaded  end
